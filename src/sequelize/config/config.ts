5244c283441b9dfb72a41095a3f67dd58e787957
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2025-02-05 16:16:09 +0100

    🚑 production db config

--- src/sequelize/config/config.ts
+++ src/sequelize/config/config.ts
@@ -26,11 +26,11 @@ const dbConfig: { [key in Env]: SequelizeOptions } = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_PROD_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
     dialect: 'postgres',
+    host: process.env.DB_PROD_HOST,
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
     models: [process.cwd() + '/build/sequelize/models'],
   },
 }
